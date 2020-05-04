979d7e5d87e13596fcc63617a4b8800e2f3bce21
Milosz Gawel <mgawel@gmail.com>
2020-05-04 15:36:04 +0200

    Updates

---
+++ precache-manifest.149cd67a9ad9e293c7b28b13250d42e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2f8959067350dee48c7624a3a610f2",
+    "url": "/companyble/index.html"
+  },
+  {
+    "revision": "75d76bd55f7db1870238",
+    "url": "/companyble/static/js/2.533ff538.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/companyble/static/js/2.533ff538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae5a806905afb7d366f3",
+    "url": "/companyble/static/js/main.68ec3346.chunk.js"
+  },
+  {
+    "revision": "89fb7a56e55f2d76284b",
+    "url": "/companyble/static/js/runtime-main.bcdfab14.js"
+  },
+  {
+    "revision": "ed8573af30f2138824b5bf86d70b459d",
+    "url": "/companyble/static/media/image.ed8573af.png"
+  }
+]);
